dddea2a1dfadc57e601e93cf5d33c71747af691b
woomin0225 <rkddnals1997@gmail.com>
2026-02-10 23:20:30 +0900

    Initial commit: backend and frontend project setup

---
+++ backend_workspace/src/test/java/org/team4p/woorizip/BackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.team4p.woorizip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
