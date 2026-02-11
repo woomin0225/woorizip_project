ffb8cbe6fff920c25ffe19205f544e0b1bc39196
Jae-Sang <aa0338@naver.com>
2026-02-11 15:17:32 +0900

    HouseService 와 HouseServiceImpl 구조 작성

--- backend_workspace/src/main/java/org/team4p/woorizip/house/service/HouseService.java
+++ backend_workspace/src/main/java/org/team4p/woorizip/house/service/HouseService.java
@@ -1,5 +1,19 @@
 package org.team4p.woorizip.house.service;
 
-public interface HouseService {
+import java.util.List;
 
+import org.team4p.woorizip.house.dto.HouseDto;
+import org.team4p.woorizip.house.dto.response.HouseMarkerResponse;
+import org.team4p.woorizip.room.dto.request.RoomSearchCondition;
+
+public interface HouseService {
+	
+	HouseDto selectHouses(RoomSearchCondition cond);
+	HouseMarkerResponse selectHouseMarkers(RoomSearchCondition cond);
+	List<HouseDto> selectHousesByOwnerNo(String userNo);
+	HouseDto selectHouse(String houseNo);
+	HouseDto insertHouse(HouseDto houseDto);
+	HouseDto updateHouse(HouseDto houseDto);
+	int deleteHouse(String houseNo);
+	
 }
--- backend_workspace/src/main/java/org/team4p/woorizip/house/service/HouseServiceImpl.java
+++ backend_workspace/src/main/java/org/team4p/woorizip/house/service/HouseServiceImpl.java
@@ -1,8 +1,59 @@
 package org.team4p.woorizip.house.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.team4p.woorizip.house.dto.HouseDto;
+import org.team4p.woorizip.house.dto.response.HouseMarkerResponse;
+import org.team4p.woorizip.house.jpa.repository.HouseRepository;
+import org.team4p.woorizip.room.dto.request.RoomSearchCondition;
 
 @Service
 public class HouseServiceImpl implements HouseService {
+	private final HouseRepository houseRepository;
+
+	@Override
+	public HouseDto selectHouses(RoomSearchCondition cond) {
+		// 건물 검색 결과 조회
+		return null;
+	}
+
+	@Override
+	public HouseMarkerResponse selectHouseMarkers(RoomSearchCondition cond) {
+		// 지도 내 건물 마커용 검색 결과 조회
+		return null;
+	}
+
+	@Override
+	public List<HouseDto> selectHousesByOwnerNo(String userNo) {
+		// 임대인 회원 건물 목록 조회
+		return null;
+	}
+
+	@Override
+	public HouseDto selectHouse(String houseNo) {
+		// 건물 상세 조회
+		return null;
+	}
+
+	@Override
+	public HouseDto insertHouse(HouseDto houseDto) {
+		// 건물 등록
+		return null;
+	}
+
+	@Override
+	public HouseDto updateHouse(HouseDto houseDto) {
+		// 건물 정보 수정
+		return null;
+	}
+
+	@Override
+	public int deleteHouse(String houseNo) {
+		// 건물 정보 삭제
+		return 0;
+	}
+	
+	
 
 }
