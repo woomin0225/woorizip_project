712a96002828233c7fdf56a3c43e651bf8c817bc
Jae-Sang <aa0338@naver.com>
2026-02-11 16:03:56 +0900

    Update HouseImageDto.java

--- backend_workspace/src/main/java/org/team4p/woorizip/house/image/dto/HouseImageDto.java
+++ backend_workspace/src/main/java/org/team4p/woorizip/house/image/dto/HouseImageDto.java
@@ -1,5 +1,13 @@
 package org.team4p.woorizip.house.image.dto;
 
-public class HouseImageDto {
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
+public class HouseImageDto {
+	private int houseImageNo;
+	private String houseNo;
+	private String houseOriginalImageName;
+	private String houseStoredImageName;
 }
