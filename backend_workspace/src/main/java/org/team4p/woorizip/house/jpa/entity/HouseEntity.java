d204ce94abcbcb550b95dc7711cbded478da08ee
Jae-Sang <aa0338@naver.com>
2026-02-11 14:54:22 +0900

    House HouseEntity 작성

--- backend_workspace/src/main/java/org/team4p/woorizip/house/jpa/entity/HouseEntity.java
+++ backend_workspace/src/main/java/org/team4p/woorizip/house/jpa/entity/HouseEntity.java
@@ -1,10 +1,91 @@
 package org.team4p.woorizip.house.jpa.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.Builder;
 
 @Builder
-@Table(name="tb_houses")
+@Table(name = "tb_houses")
+@Entity
 public class HouseEntity {
 
+	@Id
+	@Column(name = "house_no")
+	private String houseNo;
+
+	@Column(name = "house_name")
+	private String houseName;
+
+	@Column(name = "user_no")
+	private String userNo;
+
+	@Column(name = "house_created_at")
+	private LocalDateTime houseCreatedAt;
+
+	@Column(name = "house_updated_at")
+	private LocalDateTime houseUpdatedAt;
+
+	@Column(name = "house_updated_at")
+	private String houseZip;
+	
+	@Column(name="house_address")
+	private String houseAddress;
+	
+	@Column(name="house_address_detail")
+	private String houseAddressDetail;
+	
+	@Column(name="house_completion_year")
+	private Integer houseCompletionYear;
+	
+	@Column(name="house_floors")
+	private Integer houseFloors;
+	
+	@Column(name="house_house_holds")
+	private Integer houseHouseholds;
+	
+	@Column(name="house_elevator_yn")
+	private Boolean houseElevatorYn;
+	
+	@Column(name="house_pet_yn")
+	private Boolean housePetYn;
+	
+	@Column(name="house_female_limit")
+	private String houseFemaleLimit;
+	
+	@Column(name="house_parking_max")
+	private Integer houseParkingMax;
+	
+	@Column(name="house_abstract")
+	private String houseAbstract;
+	
+	@Column(name="house_image_count")
+	private Integer houseImageCount;
+	
+	@Column(name="house_lat")
+	private Double houseLat;
+	
+	@Column(name="house_lng")
+	private Double houseLng;
+	
+	@Column(name="deleted")
+	private Boolean deleted;
+	
+	@Column(name="deleted_at")
+	private LocalDateTime deleted_at;
+
+	@PrePersist
+	public void prePersist() {
+		if (houseNo == null)
+			houseNo = java.util.UUID.randomUUID().toString();
+		
+		// houseCreatedAt은 Patch에선 없어도 되므로 작성 안함
+		
+		if (houseUpdatedAt == null)
+			houseUpdatedAt = LocalDateTime.now();
+	}
 }
