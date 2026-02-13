5f3abbff7ad29e7c3cc96da51451fe35acfff4ee
Jae-Sang <aa0338@naver.com>
2026-02-13 09:51:08 +0900

    RoomSearchCondition 검색 조건 전달용 Dto 클래스 작성

--- backend_workspace/src/main/java/org/team4p/woorizip/room/dto/request/RoomSearchCondition.java
+++ backend_workspace/src/main/java/org/team4p/woorizip/room/dto/request/RoomSearchCondition.java
@@ -1,5 +1,22 @@
 package org.team4p.woorizip.room.dto.request;
 
-public class RoomSearchCondition {
+import java.util.List;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class RoomSearchCondition {
+	private String keyword;	// 검색 키워드
+	private int minTax;	// 최소 월세액
+	private int maxTax;	// 최대 월세액
+	private int minDeposit;	// 최소 보증금
+	private int maxDeposit;	// 최대 보증금
+	private double swLat;	// bbox 남서 위도
+	private double swLng;	// bbox 남서 경도
+	private double neLat;	// bbox 북동 위도
+	private double neLng;	// bbox 북동 경도
+	private String roomType;	// 전세, 월세 구분
+	private List<String> options;	// 가구옵션
 }
