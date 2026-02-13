c0f7d8f397ed701da2c84c017b92dbfeaa7e16e0
woomin0225 <rkddnals1997@gmail.com>
2026-02-13 15:55:15 +0900

    User 누락된 클래스 추가

--- backend_workspace/src/main/java/org/team4p/woorizip/user/model/service/UserService.java
+++ backend_workspace/src/main/java/org/team4p/woorizip/user/model/service/UserService.java
@@ -10,7 +10,7 @@ public interface UserService {
     int selectCheckEmailId(String emailId);
 
     // 회원 정보 조회
-    UserDto selectUser(String emailId);
+    UserDto selectUser(UserDto userDto);
 
     // 회원 가입
     int insertUser(UserDto userDto);
@@ -24,15 +24,11 @@ public interface UserService {
     // 회원 목록 조회
     List<UserDto> selectList(Pageable pageable);
 
-    // 아이디 검색 목록 갯수 조회
-    int selectSearchEmailCount(String keyword);
+    // 유저 검색 목록 갯수 조회
+    int selectSearchListCount(UserDto userDto);
 
-    // 나이 검색 목록 갯수 조회
-    int selectSearchAgeCount(int age);
+    // 유저 검색 목록 조회
+    List<UserDto> selectSearchList(UserDto userDto, Pageable pageable);
 
-    // 아이디 검색 목록 조회
-    List<UserDto> selectSearchEmailId(String keyword, Pageable pageable);
 
-    // 나이 검색 목록 조회
-    List<UserDto> selectSearchAge(int age, Pageable pageable);
 }
