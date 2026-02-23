bcdfd1a13e2e6aa205c29f8874f27fbf2fd901f9
Jae-Sang <aa0338@naver.com>
2026-02-23 11:39:10 +0900

    건물, 방 EndpointPolicy 추가/정리

--- backend_workspace/src/main/java/org/team4p/woorizip/auth/security/authorization/EndpointPolicy.java
+++ backend_workspace/src/main/java/org/team4p/woorizip/auth/security/authorization/EndpointPolicy.java
@@ -9,7 +9,20 @@ public final class EndpointPolicy {
             	"/api/information/**",
             	"/api/event/**",
             	"/api/qna/**",
-            "/api/boards/**"
+            "/api/boards/**",
+            
+            // PUBLIC_GET: house, room
+            "/houses/**",
+//            "/houses/marker",
+//            "/houses/*/rooms",
+//            "/houses/*",
+//            "/houses/*/images",
+//            "/houses/*/search",
+            "/rooms/**",
+//            "/rooms/search",
+//            "/rooms/*",
+//            "/rooms/*/images",
+//            "/rooms/*/reviews",
     };
 
     public static final String[] PUBLIC_POST = {
@@ -36,4 +49,26 @@ public final class EndpointPolicy {
     public static final String[] USER_ME = { "/api/user/*" };
     public static final String[] USER_ADMIN_LIST = { "/api/user", "/api/user/search" };
     public static final String[] USER_ADMIN_PATCH = { "/api/user/*/login-ok" };
+    
+    // 건물, 방 (ESTATE): LESSOR (GET) - PUBLIC_GET의 /houses/*와 충돌 주의 => 먼저 설정하기
+    public static final String[] ESTATE_LESSOR_GET = {
+    		"/houses/owner"
+    	};
+    // 건물, 방 (ESTATE): LESSOR (POST, PUT, PATCH, DELETE)
+    public static final String[] ESTATE_LESSOR = {
+    		"/houses", "/houses/**",
+    		"/rooms", "/rooms/**",
+//    		"/houses", "/rooms",	//POST
+//    		"/houses/*", "/rooms/*",	//PUT
+//    		"/rooms/*/availability",	//PATCH
+//    		"/houses/*", "/rooms/*"	//DELETE
+    	};
+    // 건물, 방 (ESTATE): USER (POST, PUT, DELETE)
+    public static final String[] ESTATE_USER = {
+    		"/rooms/*/reviews", "/rooms/*/reviews/*",
+//    		"/rooms/*/reviews",	//POST
+//    		"/rooms/*/reviews/*",	//PUT
+//    		"/rooms/*/reviews/*"	//DELETE
+    	};
+    
 }
