9ff9e71aa2f0a52dbb492f5175a700f72f0c246a
Jae-Sang <aa0338@naver.com>
2026-02-25 14:32:16 +0900

    프론트-건물,방: 매물관리(Management.jsx) 구조 작성

--- frontend_workspace/src/features/houseAndRoom/pages/Management.jsx
+++ frontend_workspace/src/features/houseAndRoom/pages/Management.jsx
@@ -5,12 +5,12 @@ export default function Management(){
             <div>
                 <table>
                     <tr>
-                        <td><button onClick={}>건물 등록</button></td>
-                        <td><button onClick={}>방 등록</button></td>
+                        <td><Link to={""}><button >건물 등록</button></Link></td>
+                        <td><Link to={""}><button >방 등록</button></Link></td>
                     </tr>
                     <tr>
-                        <td><button onClick={}>목록조회/수정</button></td>
-                        <td><button onClick={}>삭제</button></td>
+                        <td><Link to={""}><button >목록조회/수정</button></Link></td>
+                        <td><Link to={""}><button >삭제</button></Link></td>
                     </tr>
                 </table>
             </div>
