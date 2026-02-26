3cd411029f61f8b2fabe1ff78ed9e81953264e4a
Jae-Sang <aa0338@naver.com>
2026-02-26 15:59:19 +0900

    프론트-새로고침시 api호출 401에러 해결완료

--- frontend_workspace/src/App.js
+++ frontend_workspace/src/App.js
@@ -4,19 +4,8 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './assets/css/argon-design-system-react.min.css';
 import AuthProvider from './app/providers/AuthProvider';
 import AppRouter from './app/router';
-import { useEffect } from 'react';
-import { initHttpClient } from './app/http/request';
 
 function App() {
-  useEffect(()=>{
-    initHttpClient({
-      onLogout: () => {
-        // 토큰 정리 후 UI 갱신이 필요하면
-        window.dispatchEvent(new Event("profile-updated"));
-      },
-    });
-  });
-  
   return (
     <AuthProvider>
       <AppRouter />
