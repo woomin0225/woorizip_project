93c1ef96a1e50b43d0a99dc2cf11b7221ccfd998
Jae-Sang <aa0338@naver.com>
2026-03-12 12:47:26 +0900

    openai 제거

--- ai_server/app/clients/embedding_client.py
+++ ai_server/app/clients/embedding_client.py
@@ -6,21 +6,6 @@ from app.core.config import settings
 from openai import OpenAI
 
 
-
-# text-embedding-3-small client 생성
-class OpenaiEmbeddingClient:
-    def __init__(self):
-        self.client=OpenAI(api_key=settings.OPENAI_API_KEY)
-        self.model_name="text-embedding-3-small"
-        
-    def embed(self, text:str):
-        response=self.client.embeddings.create(
-            input=text,
-            model=self.model_name
-        )
-        vector=response.data[0].embedding
-        return vector
-    
 # nlpai-lab/KURE-v1
 class KureEmbeddingClient:
     def __init__(self):
